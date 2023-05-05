479af680febba0f955a44c0830ef41ad9ac786e0
unknown <magesh.vlb@gmail.com>
2023-05-05 16:38:17 +0530

    Initialized git repor with cookie cutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is NLP project for text classification using Hugging Face',
+    author='Magesh Kuppusamy',
+    license='MIT',
+)
